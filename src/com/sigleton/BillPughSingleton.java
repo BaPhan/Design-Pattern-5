d104a5847823c232b298985dd31ddafea597ae8b
BaPhan <phanvanba110498@gmail.com>
2023-01-28 22:07:39 +0700

    oooooo

--- src/com/sigleton/BillPughSingleton.java
+++ src/com/sigleton/BillPughSingleton.java
@@ -1,2 +1,14 @@
-package com.sigleton;public class BillPughSingleton {
+package com.sigleton;
+
+public class BillPughSingleton {
+    private BillPughSingleton(){
+
+    }
+    public static BillPughSingleton getInstance(){
+        return SingletonHelper.INSTANCE;
+    }
+    //nested class
+    private static class SingletonHelper {
+        private static final BillPughSingleton INSTANCE = new BillPughSingleton();
+    }
 }
--- src/com/sigleton/DoubleCheckLockingSingleton.java
+++ src/com/sigleton/DoubleCheckLockingSingleton.java
@@ -1,2 +1,23 @@
-package com.sigleton;public class DoubleCheckLockingSingleton {
+package com.sigleton;
+
+public class DoubleCheckLockingSingleton {
+    private static volatile DoubleCheckLockingSingleton instance;
+
+    private DoubleCheckLockingSingleton(){}
+
+    public static DoubleCheckLockingSingleton getInstance(){
+        // Do something before get instance ...
+        if (instance == null) {
+            //Do the task too long before create instance ...
+            //Block so other threads connot come into while init
+            synchronized (DoubleCheckLockingSingleton.class){
+                //re-check again,maybe another thread has init before
+                if (instance == null){
+                    instance = new DoubleCheckLockingSingleton();
+                }
+            }
+        }
+        //do something after get instance ...
+        return instance;
+    }
 }
